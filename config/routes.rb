aacb6a9191a27d2caf9d33f4619981f25feb2cad
Hauptmannhuy <dindago2324@gmail.com>
2024-05-11 18:52:38 +0300

    initial sketch of user routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources "user_search", only: [:index]
+  resources :users, only: [:index,:create,:show] 
+  root "users#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
