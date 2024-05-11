3965bccea929c534535ab9401b4edab57cc10280
Hauptmannhuy <dindago2324@gmail.com>
2024-05-11 19:48:19 +0300

    expanded users resources, outlined turbo-frames work in users#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :users, only: [:index,:create,:show]
+    resources :users, only: [:index,:show]
+    get "friends/list", to: "friends#index"
+    resource :friends, only: [:destroy,:show] do 
+     resources :requests, only: [:index, :create,:destroy] 
+    end
+
+
   root "users#index"
   # Defines the root path route ("/")
   # root "posts#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_11_152251) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_11_165318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_11_152251) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user", default: "", null: false
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
