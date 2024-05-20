2e91f6864cc9fc07e5d80e6d80fc2b5638b2866e
Hauptmannhuy <dindago2324@gmail.com>
2024-05-20 19:54:39 +0300

    implemented creation new friendship records and deletion requests

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,14 @@
 class FriendshipsController < ApplicationController
   def create
-    
+    current_user.requests.destroy_by(request_receiver_id: permitted_params[:friend_a_id], request_sender_id: permitted_params[:friend_b_id])
+    Friendship.create!(permitted_params)
   end
 
   def destroy
 
   end
-
+  private
+  def permitted_params
+    params.require(:friendship).permit(:friend_a_id,:friend_b_id)
+  end
 end
