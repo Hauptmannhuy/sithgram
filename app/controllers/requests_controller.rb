fb674d3e1ad7f8fc3256cbdc1ae1227f147f6e0d
Hauptmannhuy <dindago2324@gmail.com>
2024-05-20 18:43:13 +0300

    corrected proper filling out of parameters when sending a friend request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,17 +1,20 @@
 class RequestsController < ApplicationController
   def index
+    @user = current_user
+    @requests = current_user.requests
   end
   def destroy
   end
 
   def create
-    @receiver = current_user
+    @receiver = User.find(permitted_params[:request_receiver_id])
+    binding.pry
     @receiver.requests.create!(permitted_params)
   end
 
 
   private
  def permitted_params
-  params.require(:request).permit(:request_sender_id)
+  params.require(:request).permit(:request_receiver_id,:request_sender_id)
  end
 end
