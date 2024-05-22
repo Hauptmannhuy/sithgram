56cbdfa0470f71469a522634b41872581d823f18
Hauptmannhuy <dindago2324@gmail.com>
2024-05-22 15:48:20 +0300

    created basic index page for posts with initial styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_21_135118) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_120357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_21_135118) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "body"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
