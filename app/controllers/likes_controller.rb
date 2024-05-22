d59902fd91a4a79f44346199e584ccf58c287e93
Hauptmannhuy <dindago2324@gmail.com>
2024-05-22 19:25:15 +0300

    created basic feature to like a post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,18 @@
 class LikesController < ApplicationController
-  def add
+  def create
+    @like = Like.create(permitted_params)
+    @likes = Post.find(permitted_params[:post_id]).likes.count
     
+    respond_to do |format|
+    format.turbo_stream { render turbo_stream: turbo_stream.update('count', @likes) }
+    end
   end
 
-  def remove
+  def destroy
 
+  end
+  private
+  def permitted_params
+    params.require('like').permit(:post_id,:user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,21 @@
 Rails.application.routes.draw do
-  get 'likes/add'
-  get 'likes/remove'
-  get 'posts/index'
-  get 'posts/destroy'
-  get 'posts/edit'
-  get 'friendship/create'
-  get 'friendship/destroy'
-  get 'requests/index'
-  get 'requests/create'
-  get 'requests/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-    resources :users, only: [:index,:show]
-    get "friends/list", to: "friends#list"
-    resource :friends, only: [:destroy,:show] do 
-     resources :requests, only: [:index, :create,:destroy] 
-    end
-    resources :friendships, only: [:create,:destroy]
-    resources :posts
-
+  resources :users, only: [:index,:show]
+  get "friends/list", to: "friends#list"
+  resource :friends, only: [:destroy,:show] do 
+    resources :requests, only: [:index, :create,:destroy] 
+  end
+  resources :friendships, only: [:create,:destroy]
+  resources :posts
+  
+  # post 'likes/add'
+  # post 'likes/remove'
+  resources :likes, only: [:destroy, :create]
   root "users#index"
   # Defines the root path route ("/")
   # root "posts#index"
