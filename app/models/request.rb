a025ed94cd5b4fd9c5e1a1d492811fa81575930b
Hauptmannhuy <dindago2324@gmail.com>
2024-05-28 22:55:27 +0300

    reconfigured request model to get access to both sender and receiver

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,4 @@
 class Request < ApplicationRecord
-  belongs_to :user, foreign_key: :request_receiver_id
+  belongs_to :receiver, foreign_key: :request_receiver_id, class_name: 'User'
+  belongs_to :sender, foreign_key: :request_sender_id, class_name: 'User'
 end
