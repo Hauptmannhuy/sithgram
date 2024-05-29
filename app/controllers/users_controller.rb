a120443fa08cffcf56d5da5a73788fe38ac7d1ed
hauptmannhuy <dindago2324@gmail.com>
2024-05-29 22:20:29 +0300

    skeleton for canceling request button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def index
     @users = current_user.find_not_friends_of_user
+    @requested_users = current_user.sent_requests.pluck(:request_receiver_id)
+    binding.pry
   end
 
   def show
