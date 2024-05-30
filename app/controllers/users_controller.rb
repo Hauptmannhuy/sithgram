42c1c864ce1dc864ea4b8b227fcd17c0a12b036a
hauptmannhuy <dindago2324@gmail.com>
2024-05-30 19:18:10 +0300

    added feature indicating that other user already requested current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = current_user.find_not_friends_of_user
+    @pending_users = current_user.requests.pluck(:request_sender_id)
     @requested_users = Hash.new
     current_user.sent_requests.where('request_receiver_id != ?', current_user.id).pluck(:request_receiver_id,:id).each{|el| @requested_users[el.first] = el.last}
   end
@@ -9,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = User.find(params[:id]).posts
     @friends = @user.friends.count
-    
+
   end
 
 end
