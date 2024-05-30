745adc65c392c8f8e63cfc7aec1052c58af79902
hauptmannhuy <dindago2324@gmail.com>
2024-05-30 18:24:29 +0300

    added more details about user in personal page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = User.find(params[:id]).posts
+    @friends = @user.friends.count
+    
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_175628) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_151931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_24_175628) do
     t.string "avatar_url"
     t.string "uid"
     t.string "provider"
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
