55ca0767403ea28ab6074661aef5a192f1139458
hauptmannhuy <dindago2324@gmail.com>
2024-05-30 15:11:16 +0300

    added functional cancel request button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def index
     @users = current_user.find_not_friends_of_user
-    @requested_users = current_user.sent_requests.pluck(:request_receiver_id)
+    @requested_users = Hash.new
+    current_user.sent_requests.where('request_receiver_id != ?', current_user.id).pluck(:request_receiver_id,:id).each{|el| @requested_users[el.first] = el.last}
   end
 
   def show
