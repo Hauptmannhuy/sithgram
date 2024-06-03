c444be21c4a5250919bd41afa0ee687ffd2fa98b
hauptmannhuy <dindago2324@gmail.com>
2024-06-03 17:41:03 +0300

    added search form for searching users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
   def index
-    @users = current_user.find_not_friends_of_user
+    @users = !params[:search].nil? ? User.where(username: params[:search]) : current_user.find_not_friends_of_user
     @pending_users = current_user.requests.pluck(:request_sender_id)
     @requested_users = Hash.new
     current_user.sent_requests.where('request_receiver_id != ?', current_user.id).pluck(:request_receiver_id,:id).each{|el| @requested_users[el.first] = el.last}
+    
   end
 
   def edit
     @user = User.find(params[:id])
-    
   end
 
   def update
