d2eb6c6384dd1821c93425b25a6145bbfd6e0094
hauptmannhuy <dindago2324@gmail.com>
2024-06-06 14:43:10 +0300

    configured rspec to use devise test helper methods, added request unit tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
+
 RSpec.describe "Users", type: :request do
+  let(:user){User.create(username:'test',password:'123456',email:'test@email.com')}
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    context 'when authenticated' do
+    it "returns 200 status code" do
+      sign_in user
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+  end
+  context 'when not authenticated' do 
+    it 'returns 302 status code' do 
       get users_path
       expect(response).to have_http_status(302)
     end
   end
 end
+  
+end
+
