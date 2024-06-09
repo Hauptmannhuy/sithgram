90eca036130b08ad402c21be7d7a3d3a7fb23eed
hauptmannhuy <dindago2324@gmail.com>
2024-06-09 15:29:18 +0300

    created basic unit tests for checking if page loaded properly

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,15 +3,45 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   let(:user){User.create(username:'test',password:'123456',email:'test@email.com')}
-  describe "GET /users" do
-    context 'when authenticated' do
-    it "returns 200 status code" do
+  describe "Authenticated user" do
+    context 'when visiting /users' do
+    it "returns 200 status code and displays content" do
       sign_in user
       get users_path
+      expect(response.body).to include("#{user.username}")
       expect(response).to have_http_status(200)
     end
   end
-  context 'when not authenticated' do 
+  context 'when visiting /friends/list' do
+    it "returns 200 status code and displays content" do
+      sign_in user
+      get friends_list_path
+      expect(response.body).to include("Friends")
+      expect(response).to have_http_status(200)
+    end
+  end
+  context 'when visiting /users/id' do
+    it "returns 200 status code and displays content" do
+      sign_in user
+      get user_path(user)
+      expect(response.body).to include("#{user.username}")
+
+      expect(response).to have_http_status(200)
+    end
+  end
+  context 'when visiting /posts' do
+    it "returns 200 status code and displays content" do
+      sign_in user
+      get posts_path
+      notif = "You haven't created any posts or followed someone to see feed yet."
+      expect(response.body).to include(notif)
+      expect(response).to have_http_status(200)
+    end
+  end
+end
+describe "Not authenticated user" do
+
+  context 'when visiting /users' do 
     it 'returns 302 status code' do 
       get users_path
       expect(response).to have_http_status(302)
