be157bd62272670c3f76b15e3d7d2b6bc7f1e578
hauptmannhuy <dindago2324@gmail.com>
2024-06-10 16:03:45 +0300

    added enhancing styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   has_one_attached :image
+
+  validates :body, presence: true
+  
+
  def user_liked?(user_id)
   return true if !self.likes.where(user_id:user_id).empty?
   false
